cf56fb8486e3ea526e0bc8781d55d085198ee222
Bashir Yesufu <86322263+BashirYesufu@users.noreply.github.com>
2021-11-09 00:13:15 +0100

    separated concerns for the icon contents and reusable card

---
+++ bmi_app/lib/icon_content.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class IconContent extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  const IconContent({Key? key, required this.icon, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          size: 80.0,
+        ),
+        SizedBox(height: 15.0,),
+        Text(
+          text,
+          style: TextStyle(
+            color: Color(0xFF8D8E98),
+            fontSize: 18.0,
+          ),
+        ),
+      ],
+    );
+  }
+}
--- bmi_app/lib/input_page.dart
+++ bmi_app/lib/input_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'reusable_card.dart';
+import 'icon_content.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColor = Color(0xFF1D1E33);
@@ -83,52 +85,3 @@ class _InputPageState extends State<InputPage> {
     );
   }
 }
-
-class IconContent extends StatelessWidget {
-  final IconData icon;
-  final String text;
-  const IconContent({Key? key, required this.icon, required this.text}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(
-          icon,
-          size: 80.0,
-        ),
-        SizedBox(height: 15.0,),
-        Text(
-          text,
-          style: TextStyle(
-            color: Color(0xFF8D8E98),
-            fontSize: 18.0,
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class ReusableCard extends StatelessWidget {
-
-  final Color colour;
-  final Widget? cardChild;
-  const ReusableCard({Key? key, required this.colour, this.cardChild}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        child: cardChild,
-        margin: EdgeInsets.all(15.0),
-        decoration: BoxDecoration(
-          color: colour,
-          borderRadius: BorderRadius.circular(10.0),
-        ),
-      ),
-    );
-  }
-}
-
