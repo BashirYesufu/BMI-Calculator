ce68c1d785fa2d7279e54acdf7251cd142c7afd8
Bashir Yesufu <86322263+BashirYesufu@users.noreply.github.com>
2021-11-08 21:35:13 +0100

    Created custom widget and added as children to the body of the scaffold

--- bmi_app/lib/input_page.dart
+++ bmi_app/lib/input_page.dart
@@ -12,9 +12,49 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: [
+          Expanded(
+              child: Row(
+                children: [
+                  ReusableCard(colour: Color(0xFF1D1E33),),
+                  ReusableCard(colour: Color(0xFF1D1E33),),
+                ],
+              ),
+          ),
+
+          ReusableCard(colour: Color(0xFF1D1E33),),
+
+          Expanded(
+            child: Row(
+              children: [
+                ReusableCard(colour: Color(0xFF1D1E33),),
+                ReusableCard(colour: Color(0xFF1D1E33),),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
+class ReusableCard extends StatelessWidget {
+
+  final Color colour;
+  const ReusableCard({Key? key, required this.colour}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        margin: const EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: colour,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+      ),
+    );
+  }
+}
+
