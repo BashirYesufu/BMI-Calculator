59c62e42bc7ef6badebce17ea2ef42cae42b092b
Bashir Yesufu <86322263+BashirYesufu@users.noreply.github.com>
2021-11-11 02:25:37 +0100

    Customised slider with slider theme

--- bmi_app/lib/input_page.dart
+++ bmi_app/lib/input_page.dart
@@ -82,18 +82,26 @@ class _InputPageState extends State<InputPage> {
                     ],
                   ),
 
-                  Slider(
-                    value: height.toDouble(),
-                    min: 120.0,
-                    max: 220.0,
-                    activeColor: Color(0xFFeb1555),
-                    inactiveColor: Color(0xFF8D8E98),
-                    onChanged: (double newValue) {
-                      setState(() {
-                        height = newValue.toInt();
-                        print(newValue);
-                      });
-                    },
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                      inactiveTrackColor: Color(0xFF8D8E98),
+                      thumbColor: Color(0xFFEB1555),
+                      thumbShape: RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                      overlayShape: RoundSliderOverlayShape(overlayRadius: 30.0),
+                      overlayColor: Color(0x29EB1555),
+                      activeTrackColor: Colors.white,
+                    ),
+                    child: Slider(
+                      value: height.toDouble(),
+                      min: 120.0,
+                      max: 220.0,
+                      inactiveColor: Color(0xFF8D8E98),
+                      onChanged: (double newValue) {
+                        setState(() {
+                          height = newValue.round(); 
+                        });
+                      },
+                    ),
                   ),
                 ],
               ),
